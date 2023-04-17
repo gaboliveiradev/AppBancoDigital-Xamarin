082e3e05696b7805e268828a033c3adaac49cf09
gaboliveiradev <g38789138@gmail.com>
2023-04-17 08:44:43 -0300

    add nova view de inicialização

--- AppBancoDigital/View/Login.xaml.cs
+++ AppBancoDigital/View/Login.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace AppBancoDigital
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
     {
-        public MainPage()
+        public Login()
         {
             InitializeComponent();
         }
