da36f06632894987d1745faf36bf9657dd582d44
gaboliveiradev <g38789138@gmail.com>
2023-05-13 12:04:09 -0300

    feat: Metodo cadastrar na DataServiceCorrentista + add para o app receber requisisões não seguras

--- AppBancoDigital/DataService/DataServiceCorrentista.cs
+++ AppBancoDigital/DataService/DataServiceCorrentista.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using AppBancoDigital.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AppBancoDigital.DataService
 {
-    public class DataServiceCorrentista
+    public class DataServiceCorrentista : DataService
     {
+        public static async Task<Correntista> Cadastrar(Correntista c)
+        {
+            var json_a_enviar = JsonConvert.SerializeObject(c);
+            string json = await DataService.PostDataToService(json_a_enviar, "/correntista/cadastrar");
+
+            //if (json == "false")
+              //return null;
+
+            Correntista correntista = JsonConvert.DeserializeObject<Correntista>(json);
+
+            return correntista;
+        }
     }
 }
